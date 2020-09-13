cb3b3d181b85b9f0b0c0f0a58d05eeac88617826
Serhiy <eserpav@gmail.com>
2020-09-13 13:04:22 +0300

    First working version

--- spidertube.py
+++ spidertube.py
@@ -1,16 +1,46 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python3 -u
+import time
 from pytube import YouTube
+
 
 text_file = open("links.txt", "r")
 lines = text_file.readlines()
 text_file.close()
 
-for line in lines[0:4]:
-    print(line)
-    yt = YouTube(line)
-    s = yt.streams.filter(file_extension='mp4').order_by('resolution').desc().first()
-    print(s)
+
+def download_stream(stream):
+    print("  Stream: ", end="")
+    print(stream)
+    downloaded = False
+    try:
+        stream.download()
+        downloaded = True
+    except:
+        print("Download failed")
+    return downloaded
+
+
+for line in lines:
+    print("URL: " + line.strip())
+    try:
+        yt = YouTube(line)
+        if yt.length < 1800:
+            for video_stream in yt.streams.filter(progressive=True).order_by('resolution').desc():
+                print("Downloading \"{0}\" {1} @ {2} fps".format(yt.title, video_stream.resolution, video_stream.fps))
+                if download_stream(video_stream):
+                    break
+            # for audio_stream in yt.streams.filter(file_extension='mp4', type='audio').order_by('bitrate').desc():
+            #     print("Downloading \"{0}\" {1}".format(yt.title, audio_stream.abr))
+            #     if download_stream(audio_stream):
+            #         break
+        else:
+            print("Skipping {0}:{1:02d} video \"{2}\"".format(int(yt.length/60), yt.length % 60, yt.title))
+    except:
+        print("Cannot open the URL")
+        time.sleep(20)
+    finally:
+        print("----------")
+        time.sleep(2)
 
 # EXAMPLE
 # yt.author  'Красный Угол'
@@ -23,8 +53,3 @@ def dump(obj):
     for attr in dir(obj):
         if hasattr(obj, attr):
             print("obj.%s = %s" % (attr, getattr(obj, attr)))
-
-# url = pl.video_urls
-# print(len(url))
-# for x in url:
-#    print(url[x])
